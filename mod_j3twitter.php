eb1d9399b0a48ce162258164bdb327ccc6e659eb
Smith0r <chris@stormxstudios.co.uk>
2013-11-07 15:05:20 +0000

    Add and include API Exchange

--- mod_j3twitter.php
+++ mod_j3twitter.php
@@ -11,6 +11,9 @@ defined('_JEXEC') or die('Restricted access');
 // Require the helper
 require_once(dirname(__FILE__).'/helper.php');
 
+// Require the Twitter API Exchange
+require_once(dirname(__FILE__).'/TwitterAPIExchange.php');
+
 // Fetch the layout
 require( JModuleHelper::getLayoutPath('mod_j3twitter'));
 
