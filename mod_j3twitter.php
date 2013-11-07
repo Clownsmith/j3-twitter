dfb260700735b52e7e46e3c1d2cc4b8153c098a8
Smith0r <chris@stormxstudios.co.uk>
2013-11-07 14:51:58 +0000

    Base Module

---
+++ mod_j3twitter.php
@@ -0,0 +1,17 @@
+<?php
+//============================================================================
+// @ mod_j3twitter
+// # Chris Smith - 7/11/2013
+// # Twitter Module for Joomla 3.2.x
+//============================================================================
+
+// No Direct Access
+defined('_JEXEC') or die('Restricted access');
+
+// Require the helper
+require_once(dirname(__FILE__).'/helper.php');
+
+// Fetch the layout
+require( JModuleHelper::getLayoutPath('mod_j3twitter'));
+
+?>
---
+++ tmpl/default.php
@@ -0,0 +1,12 @@
+<?php
+//============================================================================
+// @ mod_j3twitter
+// # Chris Smith - 7/11/2013
+// # Twitter Module for Joomla 3.2.x
+//============================================================================
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+echo "Twitter Module";
+
+?>
