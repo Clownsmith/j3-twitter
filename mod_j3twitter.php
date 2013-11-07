c08d20c3e2f4f366a2691e0a4453ac56f36014a6
Smith0r <chris@stormxstudios.co.uk>
2013-11-07 17:14:21 +0000

    Removed UnNeeded files

--- mod_j3twitter.php
+++ mod_j3twitter.php
@@ -9,8 +9,8 @@ defined('_JEXEC') or die('Restricted access');
 // Require the helper
 require_once(dirname(__FILE__).'/helper.php');
 $helper = new modJ3TwitterHelper;
-// Require the Twitter API Exchange & Autolink
-require_once(dirname(__FILE__).'/libs/TwitterAPIExchange.php');require_once(dirname(__FILE__).'/libs/Autolink.php');// Include the J3Twitter CSS$doc = JFactory::getDocument();$doc->addStyleSheet('modules/mod_j3twitter/assets/mod_j3twitter.css');
+// Require the Twitter API Exchange
+require_once(dirname(__FILE__).'/libs/TwitterAPIExchange.php');// Include the J3Twitter CSS$doc = JFactory::getDocument();$doc->addStyleSheet('modules/mod_j3twitter/assets/mod_j3twitter.css');
 // Fetch the layout
 require( JModuleHelper::getLayoutPath('mod_j3twitter'));
 ?>
