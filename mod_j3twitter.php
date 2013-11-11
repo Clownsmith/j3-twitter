be42baa3baa89b8a4b8a718bd90aaa13bddc37d4
Smith0r <chris@stormxstudios.co.uk>
2013-11-11 13:56:34 +0000

    Allow Joomla to Cache tweets

--- mod_j3twitter.php
+++ mod_j3twitter.php
@@ -10,7 +10,7 @@ defined('_JEXEC') or die('Restricted access');
 require_once(dirname(__FILE__).'/helper.php');
 $helper = new modJ3TwitterHelper;
 // Require the Twitter API Exchange
-require_once(dirname(__FILE__).'/libs/TwitterAPIExchange.php');// Include the J3Twitter CSS$doc = JFactory::getDocument();$doc->addStyleSheet('modules/mod_j3twitter/assets/mod_j3twitter.css');
+require_once(dirname(__FILE__).'/libs/TwitterAPIExchange.php');// Include the J3Twitter CSS$doc = JFactory::getDocument();$doc->addStyleSheet('modules/mod_j3twitter/assets/mod_j3twitter.css');$cache = &JFactory::getCache();if ($params->get('allow_cache') == "0"){	// Turn Cache On [Forced]	$cache->setCaching(1);}else{	// Turn Cache Off [Forced]	$cache->setCaching(0);}$tweet_num = $params->get('tweet_num');$username = $params->get('username');$oauth = $params->get('token');$oauth_secret = $params->get('token_secret');$key = $params->get('key');$key_secret = $params->get('key_secret');$tweets = $cache->call(array('modJ3TwitterHelper', 'fetchTweets'), $tweet_num, $username, $oauth, $oauth_secret, $key, $key_secret);
 // Fetch the layout
 require( JModuleHelper::getLayoutPath('mod_j3twitter'));
 ?>
