afbe2c4a5f81fcc7251a962133a6c36bf62d2b7d
Smith0r <chris@stormxstudios.co.uk>
2014-03-25 15:08:43 +0000

    Remove Stock OAuth Information

--- helper.php
+++ helper.php
@@ -10,7 +10,7 @@ class modJ3TwitterHelper
 	// # Fetch tweets
 	//======================================================================
 	public static function fetchTweets($limit = 5, $username = "", $oauth = "", $oauth_secret = "", $key = "", $key_secret="")
-	{		$settings = array(			'oauth_access_token' => "87945234-nqNYshxTr1FqQCYdhuCeE1BVb4x1BmKWiBbRzmvAo",			'oauth_access_token_secret' => "hx0CQZk3VN4Siny4bKml62yK57OkwzunMPKBL6cddxk",			'consumer_key' => "K2I1AVfYiOtc5p5jnCghA",			'consumer_secret' => "FMdFxIfwbwU29GFMxiSek15AnD6n9Z332IUPpw7CdBc"		);		$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';		$getfield = '?screen_name='. $username .'&count='. $limit;		$requestMethod = 'GET';		$twitter = new TwitterAPIExchange($settings);		$tweets = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest());
+	{		$settings = array(			'oauth_access_token' => $oauth,			'oauth_access_token_secret' => $oauth_secret,			'consumer_key' => $key,			'consumer_secret' => $key_secret		);		$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';		$getfield = '?screen_name='. $username .'&count='. $limit;		$requestMethod = 'GET';		$twitter = new TwitterAPIExchange($settings);		$tweets = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest());
 		return $tweets;
 	}
 }
--- mod_j3twitter.php
+++ mod_j3twitter.php
@@ -10,7 +10,7 @@ defined('_JEXEC') or die('Restricted access');
 require_once(dirname(__FILE__).'/helper.php');
 $helper = new modJ3TwitterHelper;
 // Require the Twitter API Exchange
-require_once(dirname(__FILE__).'/libs/TwitterAPIExchange.php');// Include the J3Twitter CSS$doc = JFactory::getDocument();$doc->addStyleSheet('modules/mod_j3twitter/assets/mod_j3twitter.css');$tweet_num = $params->get('tweet_num');$username = $params->get('username');$oauth = $params->get('token');$oauth_secret = $params->get('token_secret');$key = $params->get('key');$key_secret = $params->get('key_secret');$tweets = $helper->fetchTweets($tweet_num, $username, $oauth, $oauth_secret, $key, $key_secret);
+require_once(dirname(__FILE__).'/libs/TwitterAPIExchange.php');// Include the J3Twitter CSS$doc = JFactory::getDocument();$doc->addStyleSheet('modules/mod_j3twitter/assets/mod_j3twitter.css');$tweet_num = $params->get('tweet_num');$username = $params->get('username');$oauth = $params->get('token');$oauth_secret = $params->get('token_secret');$key = $params->get('key');$key_secret = $params->get('key_secret');if ($oauth != ""){	$tweets = $helper->fetchTweets($tweet_num, $username, $oauth, $oauth_secret, $key, $key_secret);}else{	$tweets = array();	$tweets[] = array("text" => "Please enter your OAuth Information.");}
 // Fetch the layout
 require( JModuleHelper::getLayoutPath('mod_j3twitter'));
 ?>
